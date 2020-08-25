c6bc2629cb5879bf7b1fc2df3b31135991131f69
Shy-El <shaielshmoel@gmail.com>
2020-08-26 00:31:07 +0300

    lobby room list hover

--- components/RoomList.js
+++ components/RoomList.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import Room from './Rooms/Room';
+import * as theme from '../styles/BlueTheme';
+
+const border = '5px solid black';
 
 export default ({ rooms }) => {
   console.log(rooms);
   return (
-    <table>
-      <tbody>
-        {rooms.map(room => (
-          <Room room={room}></Room>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <style jsx>
+        {`
+          table {
+            margin-left: auto;
+            margin-right: auto;
+            border-spacing: 0px;
+          }
+
+          :global(tr:nth-child(odd)) {
+            background-color: ${theme.Dark};
+            color: ${theme.Hover};
+          }
+          :global(tr:nth-child(even)) {
+            background-color: ${theme.Middle};
+            color: ${theme.Hover};
+          }
+          :global(tr:hover) {
+            background-color: ${theme.Hover};
+            color: ${theme.Middle};
+          }
+          :global(td) {
+            border: inherit;
+          }
+        `}
+      </style>
+      <table>
+        <tbody>
+          {rooms.map(room => (
+            <Room room={room}></Room>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
---
+++ styles/BlueTheme.js
@@ -0,0 +1,4 @@
+export const Light = 'white';
+export const Middle = '#e3ebf4';
+export const Dark = '#bbc8d5';
+export const Hover = '#424d58';
